d1f723ab8e41e29455e37fb57abb11180e58efb6
Truong Thuy Ky Duyen <81848246+duyen2310@users.noreply.github.com>
2025-04-02 12:39:25 -0400

    Create projects and spaceX service

---
+++ src/app/services/spacex.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SpacexService {
+
+  private apiUrl = 'https://api.spacexdata.com/v3/launches';
+
+  constructor(private http: HttpClient) {}
+
+  getLaunches(): Observable<any[]> {
+    return this.http.get<any[]>(this.apiUrl);
+  }
+
+  getLaunchesByYear(year: string): Observable<any[]> {
+    return this.http.get<any[]>(`${this.apiUrl}?launch_year=${year}`);
+  }
+}
